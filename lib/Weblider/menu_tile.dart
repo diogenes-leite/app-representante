a6a9d39cc1fced62880a52f092295155945c7bc0
Diogenes da Silva Leite <diogenes_dg_leite@hotmail.com>
2020-07-31 13:44:02 -0300

    commit tela webmenu

--- lib/Weblider/menu_tile.dart
+++ lib/Weblider/menu_tile.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:lider_app_teste/class/wl_list.dart';
 
 class MenuWidget extends StatelessWidget {
+  
+  final HomeWeb  homeWeb;
+
+  const MenuWidget({Key key, this.homeWeb}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final HomeWeb homeWeb;
-    const MenuWidget(this.homeweb);
-
-    final avatar = homeWeb.imagePath == null homeWeb.imagePath.isNotEmpty
+    final avatar = homeWeb.imagePath == null || homeWeb.imagePath.isNotEmpty
     ? CircleAvatar(child: Icon(Icons.person))
     : CircleAvatar(backgroundImage: AssetImage(homeWeb.imagePath));
     return ListTile(
       leading: avatar,
-    
-      
     );
   }
 }
