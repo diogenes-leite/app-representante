20d437a98734e5ab52eeb418711516c9ee942916
Diogenes da Silva Leite <diogenes_dg_leite@hotmail.com>
2020-08-05 15:04:13 -0300

    teste

--- lib/Weblider/MenuWeb.dart
+++ lib/Weblider/MenuWeb.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:lider_app_teste/Weblider/Dummy_lista.dart';
 import 'package:lider_app_teste/Weblider/menu_tile.dart';
+import 'package:lider_app_teste/class/wl_list.dart';
 
 class MenuWeb extends StatelessWidget {
+   final lista = DUMMY_LISTA;
   @override
+ 
   Widget build(BuildContext context) {
-    final lista = DUMMY_LISTA;
-
+ 
     return Scaffold(
         appBar: AppBar(
           title: Text('Sistemas Web Lider'),
         ),
         body: ListView.builder(
           itemCount: lista.length,
-          itemBuilder: (ctx, i) => MenuWidget(),
+          itemBuilder: (context,index){
+            return ListTile();
+          }
 
           //Text(lista.values.elementAt(i).title)),
         ));
