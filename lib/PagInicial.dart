458c6bd04e0f8df0ac3dbb7df15d6f8c5d544012
Diógenes da Silva Leite <diogenes_dg_leite@hotmail.com>
2020-08-05 22:34:08 -0300

    alterações basicas

--- lib/PagInicial.dart
+++ lib/PagInicial.dart
@@ -12,44 +12,51 @@ class _PagIncialState extends State<PagInicial> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sistemas Lider Automação'),
-        backgroundColor: Colors.green,
-      ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('LiderPdv'),
-              subtitle: Text('Sistema de vendas direto'),
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('assets/liderpdv.jpg'),
-                maxRadius: 40,
-                minRadius: 40,
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MenuLiderPdv()));
-              },
-            ),
+        appBar: AppBar(
+          title: Text(
+            'Sistemas Lider Automação',
           ),
-          Card(
-            child: ListTile(
-              title: Text('Weblider'),
-              subtitle: Text('Cadastros, financeiros e muito mais...'),
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('assets/weblider.png'),
-                maxRadius: 40,
-                minRadius: 40,
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MenuWeb()));
-              },
+          backgroundColor: Colors.blue,
+        ),
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    title: Text('LiderPDV'),
+                    subtitle: Text('Sistema de vendas direto'),
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/liderpdv.jpg'),
+                      maxRadius: 40,
+                      minRadius: 40,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MenuLiderPdv()));
+                    },
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('Weblider'),
+                    subtitle: Text('Cadastros, financeiros e muito mais...'),
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/weblider.png'),
+                      maxRadius: 40,
+                      minRadius: 40,
+                    ),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => MenuWeb()));
+                    },
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
--- lib/Weblider/MenuWeb.dart
+++ lib/Weblider/MenuWeb.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:lider_app_teste/Weblider/Dummy_lista.dart';
 
-
-
 class MenuWeb extends StatelessWidget {
-   final lista = DUMMY_LISTA;
+  final lista = DUMMY_LISTA;
   @override
- 
   Widget build(BuildContext context) {
- 
     return Scaffold(
         appBar: AppBar(
           title: Text('Sistemas Web Lider'),
         ),
         body: ListView.builder(
-          itemCount: lista.length,
-          itemBuilder: (context,index){
-            return ListTile();
-          }
+            itemCount: lista.length,
+            itemBuilder: (context, index) {
+              return ListTile();
+            }
 
-          //Text(lista.values.elementAt(i).title)),
-        ));
+            //Text(lista.values.elementAt(i).title)),
+            ));
   }
 }
